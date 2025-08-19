904333ce84c3bd41e766fae7197b037a4d8c048e
Chris Stone <chris@stoneyforest.net>
2025-08-19 12:42:52 -0400

    20250819-03

--- psakefile.ps1
+++ psakefile.ps1
@@ -2,6 +2,7 @@ Properties {
 	$ProjectRoot = Get-Location
 	$InputDir = Join-Path $ProjectRoot 'src'
 	$OutputDir = Join-Path $ProjectRoot 'build'
+	$TempDir = Join-Path $ProjectRoot 'bin'
 	$Version = $env:GITHUB_REF_NAME
 
 	if ($null -ne $Env:GITHUB_REPOSITORY) {
@@ -22,11 +23,6 @@ Task Init {
 	if ([string]::IsNullOrEmpty($env:AZURE_TENANT_ID)) { throw ('{0} must be defined' -f $_) }
 	if ([string]::IsNullOrEmpty($env:AZURE_CLIENT_ID)) { throw ('{0} must be defined' -f $_) }
 	if ([string]::IsNullOrEmpty($env:AZURE_CLIENT_SECRET)) { throw ('{0} must be defined' -f $_) }
-
-	# Create Output Directory
-	if (Test-Path -Path $OutputDir) { Remove-Item -Path $OutputDir -Force -Recurse }
-	New-Item -Path $OutputDir -ItemType Directory | Out-Null
-
 }
 
 Task Test -depends Init {
@@ -61,7 +57,17 @@ Task Test -depends Init {
 	}
 }
 
-Task Build -depends Test {
+Task Clean {
+	# Create Output Directory
+	if (Test-Path -Path $OutputDir) { Remove-Item -Path $OutputDir -Force -Recurse }
+	New-Item -Path $OutputDir -ItemType Directory | Out-Null
+
+	# Create Temp Directory
+	if (Test-Path -Path $TempDir) { Remove-Item -Path $TempDir -Force -Recurse }
+	New-Item -Path $TempDir -ItemType Directory | Out-Null
+}
+
+Task Build -depends Test, Clean {
 	# Copy ADMx Files
 	$InputBase = Get-Item -Path (Join-Path $InputDir 'Public')
 	Get-ChildItem -Path "$ProjectRoot\src\Public" -Filter "*.adm?" -Recurse | ForEach-Object {
@@ -70,11 +76,13 @@ Task Build -depends Test {
 		if (!(Test-Path -Path $destinationDir -PathType Container)) {
 			New-Item -ItemType Directory -Path $destinationDir | Out-Null
 		}
+		Write-Output ('Copying ADMx File: {0}' -f $_.Name)
 		Copy-Item -Path $_.FullName -Destination $destinationDir
 	}
 
 	# Signing Configuration
-	$SignCfg = ".\sign.json"
+	Write-Output ('Writing Signing Configuraiton')
+	$SignCfg = Join-Path $TempDir ".\sign.json"
 	@{
 		"Endpoint"               = "https://eus.codesigning.azure.net/"
 		"CodeSigningAccountName" = "nw-devops-trustedsigning"
@@ -82,9 +90,24 @@ Task Build -depends Test {
 		"CorrelationId"          = (("NTP", $ProjName, (Get-Date -Format 'yyyyMMdd')) -join '-')
 	} | ConvertTo-Json | Out-File $SignCfg
 
+	# Install NuGet, BuildTools, and Signing Client
+	Write-Output ('Getting NuGet, BuildTools, and Signing Client')
+	[uri] $NuGetUri = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+	$LfNuGet = Join-Path $TempDir $NuGetUri.Segments[-1]
+	Invoke-WebRequest -Uri $NuGetUri -OutFile $LfNuGet -UseBasicParsing
+
+	Set-Location $TempDir
+	Start-Process -FilePath $LfNuGet -ArgumentList 'install', 'Microsoft.Windows.SDK.BuildTools', '-Version 10.0.22621.3233', '-x' -NoNewWindow -Wait
+	Start-Process -FilePath $LfNuGet -ArgumentList 'install', 'Microsoft.Trusted.Signing.Client', '-Version 1.0.53', '-x' -NoNewWindow -Wait
+	Set-Location $ProjectRoot
+
+	$SignToolPath = (Get-Item -Path (Join-Path $TempDir '.\Microsoft.Windows.SDK.BuildTools\bin\10.0.22621.0\x64\signtool.exe')).FullName
+	$DlibPath = (Get-Item -Path (Join-Path $TempDir '.\Microsoft.Trusted.Signing.Client\bin\x64\Azure.CodeSigning.Dlib.dll')).FullName
+
 	# Process Private Scripts
+	Write-Output ('Gathering Private Functions')
 	$privateFunctions = @{}
-	Get-ChildItem -Path "$ProjectRoot\src\Private" -Filter "*.ps1" -Recurse | ForEach-Object {
+	Get-ChildItem -Path "$InputDir\Private" -Filter "*.ps1" -Recurse | ForEach-Object {
 		$scriptContent = Get-Content -Path $_.FullName -Raw
 		$tokens = $null; $errors = $null
 		$ast = [System.Management.Automation.Language.Parser]::ParseInput($scriptContent, [ref]$tokens, [ref]$errors)
@@ -96,9 +119,9 @@ Task Build -depends Test {
 
 	# Process Public Scripts
 	$InputBase = Get-Item -Path (Join-Path $InputDir 'Public')
-	Get-ChildItem -Path "$ProjectRoot\src\Public" -Filter "*.ps1" -Recurse | ForEach-Object {
+	Get-ChildItem -Path $InputBase -Filter "*.ps1" -Recurse | ForEach-Object {
 		# Build Destination Path, Copy
-		$destinationDir = Join-Path -Path $ProjectRoot -ChildPath "build" -AdditionalChildPath (Resolve-Path -Path $_.FullName -Relative -RelativeBasePath $InputBase | Split-Path)
+		$destinationDir = Join-Path -Path $OutputDir -ChildPath (Resolve-Path -Path $_.FullName -Relative -RelativeBasePath $InputBase | Split-Path)
 		if (!(Test-Path -Path $destinationDir -PathType Container)) {
 			New-Item -ItemType Directory -Path $destinationDir | Out-Null
 		}
@@ -149,17 +172,31 @@ Task Build -depends Test {
 			$updatedScriptContent = $updatedScriptContent.Remove($indexOfPlaceholder, $functionsPlaceholder.Length).Insert($indexOfPlaceholder, $functionsContent)
 		}
 
+		Write-Output ('Writing Public Script: {0}' -f $newItem.Name)
 		Set-Content -Path $newItem.FullName -Value $updatedScriptContent
 
 		#Sign it!
-		C:\temp\Microsoft.Windows.SDK.BuildTools\bin\10.0.22621.0\x64\signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "C:\temp\Microsoft.Trusted.Signing.Client\bin\x64\Azure.CodeSigning.Dlib.dll" /dmdf $SignCfg $newItem.FullName
+		$ArgumentList = @(
+			'sign',
+			'/v',
+			'/debug',
+			'/fd', 'SHA256',
+			'/tr', 'http://timestamp.acs.microsoft.com',
+			'/td', 'SHA256',
+			'/dlib', $DlibPath,
+			'/dmdf', $SignCfg,
+			$newItem.FullName
+		)
+		Start-Process -FilePath $SignToolPath -ArgumentList $ArgumentList -NoNewWindow -Wait
 	}
 
 	# Export Certs
+	Write-Output ('Writing Certs')
 	$SigningCert = Get-AuthenticodeSignature -FilePath (Get-ChildItem -Path $OutputDir -Filter '*.ps1' -Recurse | Select-Object -First 1).FullName
 	$SigningCert.SignerCertificate.ExportCertificatePem() | Out-File -FilePath (Join-Path $OutputDir ($SigningCert.SignerCertificate.Thumbprint + '.crt'))
 
 	# Zip for Release
+	Write-Output ('Zipping Release')
 	if ($Version) {
 		Compress-Archive -Path $OutputDir\* -DestinationPath (Join-Path $OutputDir "NuWaveCWRMMAgent-$Version.zip")
 	} else {
